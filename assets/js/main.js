b9fd9fe9924b7f6106a9c0e1b7739bebc1386d05
DYO-EndlessMemory <david.barna.b.ac@gmail.com>
2026-05-01 20:36:07 +0300

    Add portfolio carousel script

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+const carousel = document.querySelector("[data-carousel]");
+
+if (carousel) {
+  const slides = Array.from(carousel.querySelectorAll(".portfolio-slide"));
+  const dots = Array.from(carousel.querySelectorAll(".carousel-dots button"));
+  const prevButton = carousel.querySelector(".carousel-prev");
+  const nextButton = carousel.querySelector(".carousel-next");
+
+  let currentIndex = 0;
+  let autoplay;
+
+  function showSlide(index) {
+    currentIndex = (index + slides.length) % slides.length;
+
+    slides.forEach((slide, slideIndex) => {
+      slide.classList.toggle("is-active", slideIndex === currentIndex);
+    });
+
+    dots.forEach((dot, dotIndex) => {
+      dot.classList.toggle("is-active", dotIndex === currentIndex);
+    });
+  }
+
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
+
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  function startAutoplay() {
+    autoplay = window.setInterval(nextSlide, 6000);
+  }
+
+  function resetAutoplay() {
+    window.clearInterval(autoplay);
+    startAutoplay();
+  }
+
+  nextButton.addEventListener("click", () => {
+    nextSlide();
+    resetAutoplay();
+  });
+
+  prevButton.addEventListener("click", () => {
+    prevSlide();
+    resetAutoplay();
+  });
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      showSlide(index);
+      resetAutoplay();
+    });
+  });
+
+  showSlide(currentIndex);
+  startAutoplay();
+}
